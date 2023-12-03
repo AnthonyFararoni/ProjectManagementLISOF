0ae44bef1ba82d327330621376f2d046a1722455
= <=>
2023-12-03 00:24:22 -0600

    Navegacion de vistas, actualizacion en utilidades
    Se agrego la vista FXMLAssignedActivities con su respectivo controlador, ademas se agregaron algunas validaciones y metodos en Utilities

--- src/projectmanagementlisof/controller/FXMLDeveloperOptionController.java
+++ src/projectmanagementlisof/controller/FXMLDeveloperOptionController.java
@@ -1,13 +1,62 @@
 package projectmanagementlisof.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import projectmanagementlisof.utils.Utilities;
 
 public class FXMLDeveloperOptionController implements Initializable
 {
+
+    @FXML
+    private TextField tfSearchDeveloper;
+    
       @Override public void initialize(URL url, ResourceBundle rb)
       {
             // TODO
       }
+
+    @FXML
+    private void btnShowDeveloperDetails(ActionEvent event) 
+    {
+    }
+
+    @FXML
+    private void btnShowAssignedActivities(ActionEvent event) 
+    {
+        goAssignedActivities();
+    }
+    
+    private void goAssignedActivities(/*String idDeveloper, String developerName*/)
+      {
+        try {
+            FXMLLoader loader = Utilities.loadView("gui/FXMLViewAssignedActivities.fxml");
+            Parent view = loader.load();
+            Scene scene = new Scene(view);
+            FXMLViewAssignedActivitiesController controller = loader.getController();
+            
+            Stage currentStage = new Stage();
+            currentStage.setScene(scene);
+            currentStage.setTitle("Actividades asignadas");
+            currentStage.initModality(Modality.APPLICATION_MODAL);
+            currentStage.showAndWait();
+            Utilities.centerStage(currentStage);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void btnSearchDeveloper(MouseEvent event) {
+    }
 }
--- src/projectmanagementlisof/utils/Utilities.java
+++ src/projectmanagementlisof/utils/Utilities.java
@@ -1,8 +1,16 @@
 package projectmanagementlisof.utils;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -15,11 +23,51 @@ public class Utilities
             stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 2);
       }
       
+    public static FXMLLoader loadView(String pathFXML)throws IOException
+    {
+        URL url = projectmanagementlisof.ProjectManagementLISOF.class.getResource(pathFXML);
+        return new FXMLLoader(url);
+    }
+      
       public static boolean validateIdDeveloper(String idDeveloper) 
       {
-        String patron = "^(zs|zS|ZS)\\d{8}$";
-        Pattern pattern = Pattern.compile(patron);
+        String rightPattern = "^(zs|zS|ZS)\\d{8}$";
+        Pattern pattern = Pattern.compile(rightPattern);
         Matcher matcher = pattern.matcher(idDeveloper);
         return matcher.matches() && !idDeveloper.trim().isEmpty();
     }
+    
+    public static boolean validateStringsFields(String fieldToCheck)
+    {
+        String rightPattern = "^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ]+$";
+        Pattern pattern = Pattern.compile(rightPattern);
+        Matcher matcher = pattern.matcher(fieldToCheck);
+        return matcher.matches() && !fieldToCheck.trim().isEmpty();
+    }
+    
+    public static void showSimpleAlert(String title, String message, Alert.AlertType type)
+    {
+        Alert simpleAlert = new Alert(type);
+        simpleAlert.setTitle(title);
+        simpleAlert.setContentText(message);
+        simpleAlert.setHeaderText(null);
+        simpleAlert.showAndWait();
+    }
+    
+    public static boolean showConfirmationAlert(String title, String message)
+    {
+        Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationAlert.setTitle(title);
+        confirmationAlert.setContentText(message);
+        confirmationAlert.setHeaderText(null);
+        Optional<ButtonType> buttonClic = confirmationAlert.showAndWait();
+        return (buttonClic.get() == ButtonType.OK);
+    }
+    
+    public static String getFullName(String name, String lastName, String secondLastName)
+    {
+        String fullName = name + " " + lastName + " " + secondLastName;
+        return fullName;
+    }
+    
 }
