7aef0afa6f13835d56d6252a54849da3570a8941
= <=>
2023-12-02 23:15:53 -0600

    Validacion matriucula estudiante
    Se agregó la validación que revisa si un String tiene la estructura correcta de una matricula

--- src/projectmanagementlisof/utils/Utilities.java
+++ src/projectmanagementlisof/utils/Utilities.java
@@ -1,5 +1,7 @@
 package projectmanagementlisof.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -12,4 +14,12 @@ public class Utilities
             stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
             stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 2);
       }
+      
+      public static boolean validateIdDeveloper(String idDeveloper) 
+      {
+        String patron = "^(zs|zS|ZS)\\d{8}$";
+        Pattern pattern = Pattern.compile(patron);
+        Matcher matcher = pattern.matcher(idDeveloper);
+        return matcher.matches() && !idDeveloper.trim().isEmpty();
+    }
 }
