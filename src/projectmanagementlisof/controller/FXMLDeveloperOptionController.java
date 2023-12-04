4cd5db01232b2ea647b0a69015106ac2150a71a4
EdmundoRdM <128428659+EdmundoRdM@users.noreply.github.com>
2023-12-04 17:59:49 -0600

    Agregadada ventana del
    Funcionando en mi maquina jajas

--- src/projectmanagementlisof/controller/FXMLDeveloperOptionController.java
+++ src/projectmanagementlisof/controller/FXMLDeveloperOptionController.java
@@ -1,13 +1,41 @@
 package projectmanagementlisof.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import projectmanagementlisof.utils.Utilities;
 
 public class FXMLDeveloperOptionController implements Initializable
 {
+    private Utilities utilities = new Utilities();
       @Override public void initialize(URL url, ResourceBundle rb)
       {
             // TODO
       }
+
+    @FXML
+    private void btnShowDeveloperLog(ActionEvent event) {
+            try{
+            FXMLLoader loader = utilities.loadView("gui/FXMLDeveloperLog.fxml");
+            Parent view = loader.load();
+            Scene scene = new Scene(view);
+            FXMLDeveloperLogController controller = loader.getController();
+            Stage stage = new Stage();
+                        
+            stage.setScene(scene);
+            stage.setTitle("Bitacora del desarrollador");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();            
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
 }
--- src/projectmanagementlisof/utils/Utilities.java
+++ src/projectmanagementlisof/utils/Utilities.java
@@ -1,15 +1,42 @@
 package projectmanagementlisof.utils;
 
+import java.io.IOException;
+import java.net.URL;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Utilities
 {
-      public static void centerStage(Stage stage)
+    public static void centerStage(Stage stage)
       {
             Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
             stage.setX((primScreenBounds.getWidth() - stage.getWidth()) / 2);
             stage.setY((primScreenBounds.getHeight() - stage.getHeight()) / 2);
       }
+      
+    public static void loadFXML(String fxmlFile, AnchorPane anchorPane) {
+        try {
+            AnchorPane FXMLFile = FXMLLoader.load(Utilities.class.getResource(fxmlFile));
+            anchorPane.getChildren().setAll(FXMLFile);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public static void showSimpleAlert(String title, String message, Alert.AlertType type){
+        Alert alertaSimple = new Alert(type);
+        alertaSimple.setTitle(title);
+        alertaSimple.setContentText(message);
+        alertaSimple.setHeaderText(null);
+        alertaSimple.showAndWait();
+    }
+    
+    public static FXMLLoader loadView(String rutaFXML) throws IOException{
+        URL url = projectmanagementlisof.ProjectManagementLISOF.class.getResource(rutaFXML);
+        return new FXMLLoader(url);
+    }
 }
