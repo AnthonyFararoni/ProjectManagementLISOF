2c2172daeb0cd060cd86c05c59288271707f7be8
= <=>
2023-12-13 04:26:03 -0600

    Arreglo de rama
    Se soluciono el problema que generaba que no cambiara entre FXML del menu

--- src/projectmanagementlisof/model/dao/DeveloperDAO.java
+++ src/projectmanagementlisof/model/dao/DeveloperDAO.java
@@ -66,52 +66,6 @@ public class DeveloperDAO
         return answer;
     }
     
-    public static HashMap<String, Object> getDevelopersById(Integer idDeveloper)
-    {
-        HashMap<String, Object> answer = new LinkedHashMap<>();
-        answer.put("error", true);
-        Connection connectionBD = ConnectionDB.getConnection();
-        if(connectionBD != null){
-            try {
-                String query = "select d.idDeveloper, d.developerLogin, d.name, d.lastname, d.secondlastname, "
-                        + "d.email, d.enrollment, d.idProject, p.name as projectName, d.idSchoolPeriod, sp.startDate,"
-                        + "sp.endDate from developer d "
-                        + "inner join project p on d.idProject = p.idProject "
-                        + "inner join schoolperiod sp on sp.idSchoolPeriod = d.idSchoolPeriod "
-                        + "where d.enrollment = 1 and d.idDeveloper = ?";
-                PreparedStatement preparedStatement = connectionBD.prepareStatement(query);
-                preparedStatement.setInt(1, idDeveloper);  
-                ResultSet developersList = preparedStatement.executeQuery();
-                ArrayList<Developer> developers = new ArrayList<>();                
-                while(developersList.next()){
-                    Developer developer = new Developer();
-                    developer.setIdDeveloper(developersList.getInt("idDeveloper"));
-                    developer.setDeveloperLogin(developersList.getString("developerLogin"));
-                    developer.setName(developersList.getString("name"));
-                    developer.setLastName(developersList.getString("lastname"));
-                    developer.setSecondLastName(developersList.getString("secondlastname"));
-                    developer.setEmail(developersList.getString("email"));
-                    developer.setEnrollment(developersList.getBoolean("enrollment"));
-                    developer.setIdProject(developersList.getInt("idProject"));
-                    developer.setProjectName(developersList.getString("projectName"));
-                    developer.setIdSchoolPeriod(developersList.getInt("idSchoolPeriod"));
-                    developer.setStarDateSchoolPeriod(developersList.getString("startDate"));
-                    developer.setEndDateSchoolPeriod(developersList.getString("endDate"));
-                    developers.add(developer);
-                }
-                connectionBD.close();
-                answer.put("error", false);
-                answer.put("developers", developers);
-            } catch (SQLException ex) {
-                answer.put("message","Error: " + ex.getMessage());
-            }     
-        }else{
-            answer.put("message", "Hubo un error al intentar conectar con la base de datos. Intente "
-                    + "de nuevo más tarde");
-        }
-        return answer;
-    }
-    
     public static HashMap<String, Object> disableDeveloper(Integer idDeveloper)
     {
         HashMap<String, Object> answer = new HashMap<>();
