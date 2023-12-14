e7723a552b9715e7ca105c78fb453021705c30b8
= <=>
2023-12-14 01:34:16 -0600

    Arreglo
    Se arreglaron propiedaes del proyecto

--- src/projectmanagementlisof/utils/Constant.java
+++ src/projectmanagementlisof/utils/Constant.java
@@ -6,6 +6,6 @@ public class Constant
       public static final String NAME_DB = "projectmanagementlisof";
       public static final String HOSTNAME = "127.0.0.1";
       public static final String PORT = "3306";
-      public static final String USER = "anthony";
-      public static final String PASSWORD = "0112";
+      public static final String USER = "userdbPCS";
+      public static final String PASSWORD = "gomez2003+";
 }
